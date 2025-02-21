5f3352a2d890e1e57b5d49cbdacc2a2a02be7a87
zoechen0717 <zoechen0717@gmail.com>
2025-02-20 22:03:27 -0800

    first commit

--- README.md
+++ README.md
@@ -1,48 +1,49 @@
 # HW6-HMM
 
-In this assignment, you'll implement the Forward and Viterbi Algorithms (dynamic programming). 
+In this assignment, you'll implement the Forward and Viterbi Algorithms (dynamic programming).
 
+![BuildStatus](https://github.com/zoechen0717/BMI203-HW6-HMM/workflows/badge.svg?event=push)
 
 # Assignment
 
-## Overview 
+## Overview
 
 The goal of this assignment is to implement the Forward and Viterbi Algorithms for Hidden Markov Models (HMMs).
 
-For a helpful refresher on HMMs and the Forward and Viterbi Algorithms you can check out the resources [here](https://web.stanford.edu/~jurafsky/slp3/A.pdf), 
-[here](https://towardsdatascience.com/markov-and-hidden-markov-model-3eec42298d75), and [here](https://pieriantraining.com/viterbi-algorithm-implementation-in-python-a-practical-guide/). 
+For a helpful refresher on HMMs and the Forward and Viterbi Algorithms you can check out the resources [here](https://web.stanford.edu/~jurafsky/slp3/A.pdf),
+[here](https://towardsdatascience.com/markov-and-hidden-markov-model-3eec42298d75), and [here](https://pieriantraining.com/viterbi-algorithm-implementation-in-python-a-practical-guide/).
 
 
 
 
 
-## Tasks and Data 
-Please complete the `forward` and `viterbi` functions in the HiddenMarkovModel class. 
+## Tasks and Data
+Please complete the `forward` and `viterbi` functions in the HiddenMarkovModel class.
 
 We have provided two HMM models (mini_weather_hmm.npz and full_weather_hmm.npz) which explore the relationships between observable weather phenomenon and the temperature outside. Start with the mini_weather_hmm model for testing and debugging. Both include the following arrays:
-* `hidden_states`: list of possible hidden states 
-* `observation_states`: list of possible observation states 
-* `prior_p`: prior probabilities of hidden states (in order given in `hidden_states`) 
+* `hidden_states`: list of possible hidden states
+* `observation_states`: list of possible observation states
+* `prior_p`: prior probabilities of hidden states (in order given in `hidden_states`)
 * `transition_p`: transition probabilities of hidden states (in order given in `hidden_states`)
 * `emission_p`: emission probabilities (`hidden_states` --> `observation_states`)
 
 
 
-For both datasets, we also provide input observation sequences and the solution for their best hidden state sequences. 
- * `observation_state_sequence`: observation sequence to test 
-* `best_hidden_state_sequence`: correct viterbi hidden state sequence 
+For both datasets, we also provide input observation sequences and the solution for their best hidden state sequences.
+ * `observation_state_sequence`: observation sequence to test
+* `best_hidden_state_sequence`: correct viterbi hidden state sequence
 
 
 Create an HMM class instance for both models and test that your Forward and Viterbi implementation returns the correct probabilities and hidden state sequence for each of the observation sequences.
 
 Within your code, consider the scope of the inputs and how the different parameters of the input data could break the bounds of your implementation.
   * Do your model probabilites add up to the correct values? Is scaling required?
-  * How will your model handle zero-probability transitions? 
-  * Are the inputs in compatible shapes/sizes which each other? 
+  * How will your model handle zero-probability transitions?
+  * Are the inputs in compatible shapes/sizes which each other?
   * Any other edge cases you can think of?
-  * Ensure that your code accomodates at least 2 possible edge cases. 
+  * Ensure that your code accomodates at least 2 possible edge cases.
 
-Finally, please update your README with a brief description of your methods. 
+Finally, please update your README with a brief description of your methods.
 
 
 
@@ -54,7 +55,7 @@ Finally, please update your README with a brief description of your methods.
 
 [TODO] Unit Testing  <br>
   [ ] Ensure functionality on mini and full weather dataset <br>
-  [ ] Account for edge cases 
+  [ ] Account for edge cases
 
 [TODO] Packaging <br>
   [ ] Update README with description of your methods <br>
@@ -62,10 +63,10 @@ Finally, please update your README with a brief description of your methods.
   [ ] github actions (install + pytest) (optional)
 
 
-## Completing the Assignment 
+## Completing the Assignment
 Push your code to GitHub with passing unit tests, and submit a link to your repository [here](https://forms.gle/xw98ZVQjaJvZaAzSA)
 
-### Grading 
+### Grading
 
 * Algorithm implementation (6 points)
     * Forward algorithm is correct (2)
@@ -79,7 +80,7 @@ Push your code to GitHub with passing unit tests, and submit a link to your repo
     * Edge cases (1)
 
 * Style (1 point)
-    * Readable code and updated README with a description of your methods 
+    * Readable code and updated README with a description of your methods
 
 * Extra credit (0.5 points)
     * Pip installable and Github actions (0.5)
--- hmm/hmm.py
+++ hmm/hmm.py
@@ -1,7 +1,7 @@
 import numpy as np
 class HiddenMarkovModel:
     """
-    Class for Hidden Markov Model 
+    Class for Hidden Markov Model
     """
 
     def __init__(self, observation_states: np.ndarray, hidden_states: np.ndarray, prior_p: np.ndarray, transition_p: np.ndarray, emission_p: np.ndarray):
@@ -10,19 +10,19 @@ class HiddenMarkovModel:
         Initialization of HMM object
 
         Args:
-            observation_states (np.ndarray): observed states 
-            hidden_states (np.ndarray): hidden states 
-            prior_p (np.ndarray): prior probabities of hidden states 
+            observation_states (np.ndarray): observed states
+            hidden_states (np.ndarray): hidden states
+            prior_p (np.ndarray): prior probabities of hidden states
             transition_p (np.ndarray): transition probabilites between hidden states
-            emission_p (np.ndarray): emission probabilites from transition to hidden states 
-        """             
-        
+            emission_p (np.ndarray): emission probabilites from transition to hidden states
+        """
+
         self.observation_states = observation_states
         self.observation_states_dict = {state: index for index, state in enumerate(list(self.observation_states))}
 
         self.hidden_states = hidden_states
         self.hidden_states_dict = {index: state for index, state in enumerate(list(self.hidden_states))}
-        
+
         self.prior_p= prior_p
         self.transition_p = transition_p
         self.emission_p = emission_p
@@ -30,25 +30,38 @@ class HiddenMarkovModel:
 
     def forward(self, input_observation_states: np.ndarray) -> float:
         """
-        TODO 
+        TODO
 
         This function runs the forward algorithm on an input sequence of observation states
 
         Args:
-            input_observation_states (np.ndarray): observation sequence to run forward algorithm on 
+            input_observation_states (np.ndarray): observation sequence to run forward algorithm on
 
         Returns:
-            forward_probability (float): forward probability (likelihood) for the input observed sequence  
-        """        
-        
+            forward_probability (float): forward probability (likelihood) for the input observed sequence
+        """
+        # Inspired by the psedocode from the pdf file: https://web.stanford.edu/~jurafsky/slp3/A.pdf
+        # Get dimentions for probability table
+        m = len(self.observation_states)
+        n = len(self.hidden_states)
+
         # Step 1. Initialize variables
-        
-       
+        # Initialize probability table
+        self.probability_table = np.zeros((m, n))
+        for s in range(1, n)):
+            # Update probabilities based on previous states
+            self.probability_table[0, s] = self.prior_p[s] * self.emission_p[s, self.observation_states_dict[input_observation_states[0]]]
+
         # Step 2. Calculate probabilities
+        for t in range(1, m):
+            for s in range(n):
+                # Sum probabilities of all possible final states
+                self.probability_table[t, s] = np.sum(alpha[t-1, :] * self.transition_p[:, s]) * self.emission_p[s, self.observation_states_dict[input_observation_states[t]]]
 
-
-        # Step 3. Return final probability 
-        
+        # Step 3. Return final probability
+        # Sum up all probabilities and return forward probability
+        forward_probability = np.sum(probability_table[-1, :])
+        return forward_probability
 
 
     def viterbi(self, decode_observation_states: np.ndarray) -> list:
@@ -58,25 +71,41 @@ class HiddenMarkovModel:
         This function runs the viterbi algorithm on an input sequence of observation states
 
         Args:
-            decode_observation_states (np.ndarray): observation state sequence to decode 
+            decode_observation_states (np.ndarray): observation state sequence to decode
 
         Returns:
             best_hidden_state_sequence(list): most likely list of hidden states that generated the sequence observed states
-        """        
-        
+        """
+
         # Step 1. Initialize variables
-        
-        #store probabilities of hidden state at each step 
+        # store probabilities of hidden state at each step
         viterbi_table = np.zeros((len(decode_observation_states), len(self.hidden_states)))
-        #store best path for traceback
-        best_path = np.zeros(len(decode_observation_states))         
-        
-       
-       # Step 2. Calculate Probabilities
+        # store best path for traceback
+        best_path = np.zeros(len(decode_observation_states))
+        # compute initial probabilities
+        for s in range(n):
+            viterbi_table[0, s] = self.prior_p[s] * self.emission_p[s, self.observation_states_dict[decode_observation_states[0]]]
 
-            
-        # Step 3. Traceback 
+        # Step 2. Calculate Probabilities
+        # update probabilities and store best paths
+        for t in range(1, m):
+            for s in range(n):
+                prob_values = viterbi_table[t-1, :] * self.transition_p[:, s]
+                best_prev_state = np.argmax(prob_values) # find the most probable previous state
+                viterbi_table[t, s] = prob_values[best_prev_state] * self.emission_p[s, self.observation_states_dict[decode_observation_states[t]]]
+                best_path[t] = best_prev_state # store the best previous state for backtracking
 
+        # find the best final state with the highest probability
+        best_last_state = np.argmax(viterbi_table[-1, :])
+        best_hidden_state_sequence = [best_last_state]
 
-        # Step 4. Return best hidden state sequence 
-        
+        # Step 3. Traceback
+        # best hidden state sequence using the stored best path
+        for i in range(m - 1, 0, -1):
+            best_hidden_state_sequence.insert(0, best_path[i])
+
+        # Step 4. Return best hidden state sequence
+        # make index sequence to actual hidden state labels
+        best_hidden_state_sequence = [self.hidden_states[int(s)] for s in best_hidden_state_sequence]
+
+        return best_hidden_state_sequence
